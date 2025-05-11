a9ad3104554dc2730657a454a8d18387da7a569b
Tobias Mair <tobiasmair0313@yahoo.com>
2025-05-12 01:12:59 +0200

    added test-views to test entity creation in database

--- src/main/java/edu/mci/snacktrack/ui/test_view/package-info.java
+++ src/main/java/edu/mci/snacktrack/ui/test_view/package-info.java
@@ -2,6 +2,6 @@
  * This package contains reusable view-related classes for the customer view.
  */
 @NullMarked
-package edu.mci.snacktrack.ui.customer_view;
+package edu.mci.snacktrack.ui.test_view;
 
 import org.jspecify.annotations.NullMarked;
