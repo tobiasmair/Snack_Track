a7a45f4614a9bc26403b5bc6f8ffa2a8b12d3c63
Nicolas Bernhart <46866783+HipFlounder@users.noreply.github.com>
2025-05-20 20:57:56 +0200

    add first report prototype

--- src/main/java/edu/mci/snacktrack/service/implementation/RestaurantService.java
+++ src/main/java/edu/mci/snacktrack/service/implementation/RestaurantService.java
@@ -3,12 +3,17 @@ package edu.mci.snacktrack.service.implementation;
 import edu.mci.snacktrack.model.Cuisine;
 import edu.mci.snacktrack.model.Restaurant;
 import edu.mci.snacktrack.repositories.CustomerRepository;
+import edu.mci.snacktrack.repositories.OrderRepository;
 import edu.mci.snacktrack.repositories.RestaurantRepository;
 import edu.mci.snacktrack.service.RestaurantServiceInterface;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -17,7 +22,7 @@ public class RestaurantService implements RestaurantServiceInterface {
 
     private RestaurantRepository restaurantRepository;
     private CustomerRepository customerRepository;
-
+    private OrderRepository orderRepository;
 
     @Override
     public Restaurant createRestaurant(String restaurantName, Cuisine cuisine, String email, String password, String address, String vatNr) {
@@ -43,6 +48,19 @@ public class RestaurantService implements RestaurantServiceInterface {
 
     public Optional<Restaurant> findByIdWithMenu(Long id) {
         return restaurantRepository.findByIdWithMenu(id);
+    }
+
+    public Map<String, Number> getSalesStats(Long restaurantId, LocalDateTime from, LocalDateTime to) {
+        Map<String, Number> result = orderRepository.getSalesStats(restaurantId, from, to);
+
+        Number totalSales = result.getOrDefault("totalSales", 0);
+        Number orderCount = result.getOrDefault("orderCount", 0);
+
+        Map<String, Number> stats = new HashMap<>();
+        stats.put("totalSales", totalSales);
+        stats.put("orderCount", orderCount);
+
+        return stats;
     }
 
 }
